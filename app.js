75683c3da23ba9304007539badc605491f621309
Marcin Zatorski <mar3211233@gmail.com>
2020-06-11 19:15:00 +0200

    Add paging to tournaments page

--- app.js
+++ app.js
@@ -40,10 +40,47 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
+class Page {
+    constructor(number, active = false, disabled = false) {
+        this.number = number;
+        this.active = active;
+        this.disabled = disabled;
+    }
+}
+
 app.get('/', async (req, res) => {
+    const itemsPerPage = 10;
+    let page = 1
+    if (req.query.page) {
+        const parsed = parseInt(req.query.page, 10);
+        if (!isNaN(parsed) && parsed > 0) {
+            page = parsed;
+        }
+    }
+
     try {
-        const tournaments = await Tournament.find().sort({ name: 'asc'});
-        res.render('index', { tournaments });
+        let numPages = Math.ceil((await Tournament.estimatedDocumentCount()) / 10);
+        page = Math.min(page, numPages);
+
+        const pages = { 
+            previous: new Page(page - 1),
+            next: new Page(page + 1), 
+            pages: []
+        };
+        if (pages.previous.number < 1) {
+            pages.previous.disabled = true;
+        }
+        if (pages.next.number > numPages) {
+            pages.next.disabled = true;
+        }
+
+        for (let i = Math.max(1, Math.min(page - 2, numPages - 4)), j = Math.min(i + 4, numPages); i <= j; ++i) {
+            pages.pages.push(new Page(i, i == page));
+        }
+
+        const tournaments = await Tournament.find().sort({ name: 'asc'})
+            .skip((page - 1) * itemsPerPage).limit(itemsPerPage);
+        res.render('index', { tournaments, pages });
     }
     catch (error) {
         console.log(error);
