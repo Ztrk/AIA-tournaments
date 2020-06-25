298826414580c769dbcd467d12a6255d09a3ac8a
Marcin Zatorski <mar3211233@gmail.com>
2020-06-25 20:31:52 +0200

    Add password resets

--- src/accounts.js
+++ src/accounts.js
@@ -9,6 +9,7 @@ const config = require('./config.json')
 const router = express.Router();
 
 const TOKEN_LENGTH = 32;
+const BCRYPT_ROUNDS = 12;
 
 const transporter = nodemailer.createTransport({
     host: "localhost",
@@ -20,6 +21,13 @@ const transporter = nodemailer.createTransport({
         rejectUnauthorized: false
     }
 });
+
+function hasExpired(date, minutes) {
+    if (!date && !(date instanceof Date)) {
+        return true;
+    }
+    return Date.now() - date.getTime() > minutes * 60 * 1000;
+}
 
 async function sendVerificationMail(email, link) {
     const message = 'Hello,\n' +
@@ -47,8 +55,30 @@ async function sendVerificationMail(email, link) {
     });
 }
 
-async function sendPasswordResetMail() {
+async function sendPasswordResetMail(email, link) {
+    const message = 'Hello,\n' +
+        'someone tried to your password in Tournaments App.\n' +
+        'If it was you, please follow the link to reset the password:\n' +
+        `${link}\n` +
+        '\n' +
+        'The link is valid for 15 minutes.\n' +
+        '\n' +
+        "If it wasn't you, you can ignore this email.\n";
 
+    const htmlMessage = '<p>Hello,</p>' +
+        '<p>someone tried to reset your password in Tournaments App. ' +
+        'If it was you, please follow the link to reset the password:</p>' +
+        `<p><a href=${link}>${link}</a></p>` +
+        '<p>The link is valid for 15 minutes.</p>' +
+        "<p>If it wasn't you, you can ignore this email.</p>";
+
+    const info = await transporter.sendMail({
+        from: '"Tournaments App" <tournamentsApp@localhost>',
+        to: email,
+        subject: 'Password reset',
+        text: message,
+        html: htmlMessage
+    });
 }
 
 function toBase64url(str) {
@@ -123,7 +153,6 @@ router.get('/register', (req, res) => {
 });
 
 router.post('/register', async (req, res) => {
-    const bcryptRounds = 12;
     if (!req.body.username || !req.body.email 
             || !req.body.password || !req.body.confirmedPassword) {
         res.redirect(303, 'register');
@@ -142,7 +171,7 @@ router.post('/register', async (req, res) => {
         return;
     }
 
-    const passwordHash = await bcrypt.hash(req.body.password, bcryptRounds);
+    const passwordHash = await bcrypt.hash(req.body.password, BCRYPT_ROUNDS);
     const emailVerificationToken = await generateToken(TOKEN_LENGTH);
     const user = new User({ 
         username: req.body.username,
@@ -202,9 +231,9 @@ router.get('/confirm', ensureNotConfirmed);
 
 router.get('/confirm', async (req, res) => {
     const user = new User(req.session.user);
-    const tokenExpirationTime = 24 * 60 * 60 * 1000; // milliseconds
+    const tokenExpirationTime = 24 * 60; // minutes
     if (user.emailVerificationToken !== req.query.token
-            || Date.now() - user.emailVerificationTime.getTime() > tokenExpirationTime) {
+            || hasExpired(user.emailVerificationTime, tokenExpirationTime)) {
         const errorMessage = 'Token is invalid';
         res.render('error', { errorMessage, user });
         return;
@@ -232,8 +261,8 @@ router.post('/resend', async (req, res) => {
     const user = new User(req.session.user);
     try {
         const token = await generateToken(TOKEN_LENGTH);
-        const link = `${req.hostname}:3000${req.baseUrl}/confirm?token=${token}`;
-        sendVerificationMail(user.email, link);
+        const link = `http://localhost:3000${req.baseUrl}/confirm?token=${token}`;
+        await sendVerificationMail(user.email, link);
         const currentDate = Date.now();
         await user.updateOne({
             emailVerificationToken: token,
@@ -252,6 +281,122 @@ router.post('/resend', async (req, res) => {
 router.get('/logout', (req, res) => {
     req.session.destroy();
     res.redirect('back');
+});
+
+router.all('/passwordReset', ensureNotLoggedIn);
+
+router.get('/passwordReset', (req, res) => {
+    res.render('users/passwordReset', { user: req.session.user });
+});
+
+router.post('/passwordReset', async (req, res) => {
+    if (!req.body.email) {
+        res.redirect(303, 'passwordReset');
+        return;
+    }
+
+    try {
+        const user = await User.findOne({ email: req.body.email })
+
+        if (!user) {
+            res.redirect(303, 'passwordReset/emailSend');
+            return;
+        }
+
+        const token = await generateToken(TOKEN_LENGTH);
+        const link = `http://localhost:3000${req.baseUrl}/passwordReset/confirm?userId=${user.id}&token=${token}`;
+        await sendPasswordResetMail(user.email, link);
+
+        const encryptedToken = await bcrypt.hash(token, BCRYPT_ROUNDS);
+        user.passwordResetToken = encryptedToken;
+        user.passwordResetTime = Date.now();
+
+        await user.save();
+    }
+    catch (error) {
+        console.error(error);
+        res.redirect(303, 'passwordReset/emailSend');
+        return;
+    }
+
+    res.redirect(303, 'passwordReset/emailSend');
+});
+
+router.get('/passwordReset/emailSend', ensureNotLoggedIn);
+
+router.get('/passwordReset/emailSend', (req, res) => {
+    res.render('users/passwordResetEmailSend', { user: req.session.user });
+});
+
+router.all('/passwordReset/confirm', ensureNotLoggedIn);
+
+router.get('/passwordReset/confirm', (req, res) => {
+    const validationError = req.session.validationError || {};
+    delete req.session.validationError;
+    res.render('users/passwordResetConfirm', { validationError, user: req.session.user });
+});
+
+router.post('/passwordReset/confirm', async (req, res) => {
+    if (!req.body.password || !req.body.confirmedPassword) {
+        res.redirect(303, req.originalUrl);
+        return;
+    }
+
+    if (req.body.password !== req.body.confirmedPassword) {
+        req.session.validationError = { confirmedPassword: true, password: true };
+        res.redirect(303, req.originalUrl);
+        return;
+    }
+
+    if (!isPasswordValid(req.body.password)) {
+        req.session.validationError = { password: true };
+        res.redirect(303, req.originalUrl);
+        return;
+    }
+
+    if (!req.query.token || !req.query.userId 
+            || !mongoose.isValidObjectId(req.query.userId)) {
+        res.redirect(303, 'invalidToken');
+        return;
+    }
+
+    try {
+        const user = await User.findById(req.query.userId);
+        if (!user || hasExpired(user.passwordResetTime, 15)) {
+            res.redirect(303, 'invalidToken');
+            return;
+        }
+
+        const isTokenValid = await bcrypt.compare(req.query.token, user.passwordResetToken);
+
+        if (!isTokenValid) {
+            res.redirect(303, 'invalidToken');
+            return;
+        }
+
+        user.passwordHash = await bcrypt.hash(req.body.password, BCRYPT_ROUNDS);
+        user.passwordResetToken = undefined;
+        user.passwordResetTime = undefined;
+
+        await user.save();
+    }
+    catch (error) {
+        console.error(error);
+        res.redirect(303, req.originalUrl);
+        return;
+    }
+
+    res.redirect(303, 'success');
+});
+
+router.get('/passwordReset/invalidToken', ensureNotLoggedIn);
+router.get('/passwordReset/invalidToken', (req, res) => {
+    res.render('users/passwordResetInvalidToken', { user: req.session.user });
+});
+
+router.get('/passwordReset/success', ensureNotLoggedIn);
+router.get('/passwordReset/success', (req, res) => {
+    res.render('users/passwordResetSuccess', { user: req.session.user });
 });
 
 module.exports = router;
